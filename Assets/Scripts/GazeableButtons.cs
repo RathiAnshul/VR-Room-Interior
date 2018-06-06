3f6bea9ffbb34c6c436d8989331c028f9b1d7c1a
Anshul Purushottam Rathi <anshulrathi03@gmail.com>
2018-06-06 11:06:32 +0530

    Third Commit
    Third Commit

---
+++ Assets/Scripts/GazeableButtons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class GazeableButtons : GazeableObjects
+{
+    protected VRCanvas parentPanel;
+
+    private void Start()
+    {
+        parentPanel = GetComponentInParent<VRCanvas>();
+    }
+
+    public void SetButtonColor(Color buttonColor)
+    {
+        GetComponent<Image>().color = buttonColor;
+    }
+
+    public override void OnPress(RaycastHit hitInfo)
+    {
+        base.OnPress(hitInfo);
+
+        if (parentPanel != null)
+        {
+            parentPanel.SetActiveButton(this);
+        }
+        else
+        {
+            Debug.Log("Button is not a child of the parent with VR Panel component", this);
+        }
+    }
+}
---
+++ Assets/Scripts/VRCanvas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCanvas : MonoBehaviour
+{
+    private GazeableButtons currentActiveButton;
+
+    public Color unselectedColor = Color.white;
+    public Color selectedColor = Color.green;
+
+    public void SetActiveButton(GazeableButtons activeButton)
+    {
+        if (currentActiveButton != null)
+        {
+            currentActiveButton.SetButtonColor(unselectedColor);
+        }
+        if (activeButton != null && currentActiveButton != activeButton)
+        {
+            currentActiveButton = activeButton;
+            currentActiveButton.SetButtonColor(selectedColor);
+        }
+        else
+        {
+            Debug.Log("Resetting");
+            currentActiveButton = null;
+        }
+    }
+}
