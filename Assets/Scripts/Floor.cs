bb15ffe7c56cdeb859ca9d1b6b895a9dfe238175
Anshul Rathi <anshulrathi03@gmail.com>
2018-06-28 18:05:11 +0530

    28-06-2018

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : GazeableObjects
+{
+    public override void OnPress(RaycastHit hitInfo)
+    {
+        base.OnPress(hitInfo);
+        if (Player.instance.activeMode == InputMode.TELEPORT)
+        {
+            Vector3 destination = hitInfo.point;
+            destination.y = Player.instance.transform.position.y;
+            Player.instance.transform.position = destination;
+        }
+    }
+}
